f44010fe6b06e9ee908cf2d1d851e0ab61ecdbd6
Vaishnavi Borse <140414095+fluffycookie07@users.noreply.github.com>
2025-01-31 15:24:59 +0530

    Update Binarytree.cpp

--- Binarytree.cpp
+++ Binarytree.cpp
@@ -2,82 +2,75 @@
 using namespace std;
 
 class emp
-{
-  public: 
-  int id,name;
+{ 
+  int id,salary;
+  string name,add;
   emp*LC,*RC;
   public:
   void create();
   void insert(emp*root,emp*next);
   void display(emp*root);
-  }*root,e;
+}*root,e;
   
-  void emp::create() 
-  {
- 	int ch;
- 	emp*next;
-  root =new emp;
-  cout<<"enter id and name of emp:";
-  cin>>root->id>>root->name ;
-  root->LC=root->RC=NULL;
-  do{
-  cout<<"do you want to continued (0/1):";
-  cin>>ch;
- 	if(ch==1) {
+void emp::create() 
+{
+ int ch;
+ emp*next;
+ root =new emp;
+ cout<<"Enter Employee Information:";
+ cin>>root->id>>root->name>>root->add>>root->salary;
+ root->LC=root->RC=NULL;
+ do{
+    cout<<"do you want to continued (0/1):";
+    cin>>ch;
+    if(ch==1) {
     next=new emp;
     cout<<"enter new info:";
-    cin>>next->id>>next->name;
+    cin>>next->id>>next->name>>next->add>>next->salary;
     next->RC=next->LC=NULL;
     insert(root,next);
-    }
-    }while(ch==1);
-    }
+   }
+   }while(ch==1);
+}
 
 void emp::insert(emp*root,emp*next)
 {
-int ch1;
-cout<<"where you want to insert"<<root->id<<"\t L or R";
-cin>>ch1;
-if(ch1=='L'||ch1=='l')
-{
- if(root->LC =NULL)
- {
- root->LC=next;
-  }
- else 
+  int ch1;
+  cout<<"Where you want to insert"<<root->id<<"\t L or R";
+  cin>>ch1;
+  if(ch1=='L'||ch1=='l')
   {
-  insert(root->LC,next);
+    if(root->LC =NULL)
+     { 
+      root->LC=next;
+     }
+  else 
+  {
+    insert(root->LC,next);
   }
   }
-  
-  else if (ch1='R'||ch1=='r'){
-  
+  else if (ch1='R'||ch1=='r')
+  { 
    if (root->RC==NULL) 
    {
    root->RC=next;
-    } else 
-  { 
+   }
+   else 
+   { 
    insert(root->RC,next);
- 	 }
- 	}
- 	}
- 
- void emp::display(emp*root)
-  {
-  if(root==NULL)
-
- return ;		
-
- else {
- 	cout<<"\n"<<root->id<<"\t"<<root->name;
- 	display(root->RC);
- 	}
+   }
   }
-  int main()
-  {
+}
+ 
+void emp::display(emp*root)
+{
+  
+}
+int main()
+{
   int choice;
   do{
-  cout<<"\n1.create\n2.insert\n3.display";
+  cout<<"\nMenu\n1.Create\n2.Display\n3.Exit";
   cout<<"enter your choice";
   cin>>choice;
   switch(choice)
@@ -89,7 +82,6 @@ if(ch1=='L'||ch1=='l')
   case 3:exit(0);
   break;
   }
-  }
-  while(choice!=3);
+  }while(choice!=3);
  return 0;
- }
+}
